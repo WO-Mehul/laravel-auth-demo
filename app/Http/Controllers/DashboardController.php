98c3c4b363fe89fdcbcd8119679ef67e5797103b
WO-Mehul <mehulmali.webosmotic@gmail.com>
2023-05-23 09:44:27 +0530

    create dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -19,6 +20,24 @@ class DashboardController extends Controller
         // Retrieve the user's name
         $userName = $user->name;
 
-        return view('dashboard', compact('userName'));
+        // Total number of users
+        $totalUsers = User::count();
+
+        // Count of users grouped by role
+       // Count of users grouped by role
+       $usersByRole = Role::withCount('users')->get();
+
+        // Count of users created each month in the last year
+        $usersByMonth = User::selectRaw('YEAR(created_at) as year, MONTH(created_at) as month, COUNT(*) as count')
+            ->where('created_at', '>=', now()->subYear())
+            ->groupBy('year', 'month')
+            ->get();
+
+        // Average number of users created per day in the last month
+        $averageUsersPerDay = User::where('created_at', '>=', now()->subMonth())
+            ->count() / 30;
+
+
+        return view('dashboard', compact('userName', 'totalUsers', 'usersByRole', 'usersByMonth', 'averageUsersPerDay'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,40 @@
 @section('title', 'Demo | Dashboard')
 @section('content')
     <h1>Welcome {{$userName}}!</h1>
+    <div class="container mx-auto px-4">
+        <div class="py-8">
+            <div class="max-w-3xl mx-auto">
+                <div class="bg-white shadow-lg rounded-lg p-6">
+                    <h2 class="text-2xl font-bold mb-4">Dashboard</h2>
+
+                    <div class="mb-6">
+                        <h3 class="text-lg font-semibold">Total Users: {{ $totalUsers }}</h3>
+                    </div>
+
+                    <div class="mb-6">
+                        <h4 class="text-lg font-semibold">Users by Role:</h4>
+                        <ul>
+                            @foreach ($usersByRole as $role)
+                                <li>{{ $role->name }}: {{ $role->users_count }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                    <div class="mb-6">
+                        <h4 class="text-lg font-semibold">Users Created Each Month (Last Year):</h4>
+                        <ul>
+                            @foreach ($usersByMonth as $data)
+                                <li>{{ date('F Y', strtotime($data->year . '-' . $data->month . '-01')) }}: {{ $data->count }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4 class="text-lg font-semibold">Average Users Created per Day (Last Month):</h4>
+                        <p>{{ $averageUsersPerDay }} users per day</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
